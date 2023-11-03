159e0c549682bb4365b6ea133e2cfcad9a2fc192
lurentao <lurentao@126.com>
2023-11-03 14:46:59 +0800

    第一次提交代码;

---
+++ src/com/taoboshi/Dog.java
@@ -0,0 +1,14 @@
+package com.taoboshi;
+
+/**
+ * 功能描述:
+ *
+ * @version v 1.0
+ * @Author: 桃博士
+ * @Date: 2023-11-03 14:45
+ */
+public class Dog {
+
+    String name;
+    int age;
+}
---
+++ src/com/taoboshi/User.java
@@ -0,0 +1,16 @@
+package com.taoboshi;
+
+/**
+ * 功能描述:
+ *
+ * @version v 1.0
+ * @Author: 桃博士
+ * @Date: 2023-11-03 14:33
+ */
+public class User {
+
+    String name;
+    int age;
+
+
+}
